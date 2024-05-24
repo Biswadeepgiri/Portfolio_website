2a2f376b91d43a9b023e1548dd4a7410a50f50c4
Biswadeepgiri <biswadeepgiri48@gmail.com>
2024-05-24 16:33:42 +0530

    worked on the experience section of the website

---
+++ frontend/src/assets/data/experiencedata.ts
@@ -0,0 +1,40 @@
+import codealpha_logo from "../../../public/experience/codealpha.jpg";
+import support_logo from "../../../public/experience/support.png";
+
+
+
+type Experience = {
+    company_logo:Object,
+    duration:string,
+    role:string,
+    work:string[],
+}
+
+
+let experiencedata:Experience[] = [
+    {
+        company_logo:codealpha_logo,
+        duration:"Dec 2023 - Jan 2024",
+        role:"Web Developer Intern",
+        work:[
+            "Executed and delivered four distinct web development projects during my internship, showcasing versatility and proficiency in web development skills.",
+            "Worked on HTML,CSS, React Js, Node Js, Express Js and MongoDB to create seamless user experience and increase efficiency of the websites by 50%",
+        ]
+
+    },
+    {
+        company_logo:support_logo,
+        duration:"Jan 2024 - Mar 2024",
+        role:"Full Stack Web Developer Intern",
+        work:[
+            "Collaborated with the backend team, utilizing Node.js and Express.js to craft 10+ APIs for the different features of the application and to ensure smooth integration and enhanced performance of the application.",
+            "Contributed to React Js project for digital internship, digital courses, digital marketing and app/web development services using React.js and Tailwind CSS.",
+        ]
+    },
+
+]
+
+
+export default experiencedata;
+
+
---
+++ frontend/src/components/individualexperience.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import experiencedata from "../assets/data/experiencedata";
+
+type experience= {
+    company_logo:object,
+    duration:string,
+    role:string,
+    work:string[],
+
+}
+
+function AnotherComponent(prop: {company_logo:object,duration:string,role:string,work:string[]}):JSX.Element {
+    return (
+        <div className="bg-gray-600 w-4/5 h-72 m-10 p-10 rounded-2xl flex flex-row space-x-5 hover:scale-105" >
+            
+        <img src={prop.company_logo} alt="company logo" />
+        <div className="flex flex-col">
+            <div className="flex flex-row">
+            <h1 className="text-white text-xl">{prop.role}</h1>
+            <h1 className="text-white text-xl">{prop.duration}</h1>
+            </div>
+            <ol>
+                {prop.work.map(function(i){
+                    return (
+                        <li className="text-white">{i}</li>
+                    )
+                })}
+            </ol>
+        </div>
+        </div>
+        
+    )
+}
+
+function IndividualExperience(prop:experience):JSX.Element {
+    return (
+        <>
+
+            {experiencedata.map(function(i){
+                return (
+                    <AnotherComponent company_logo={i.company_logo} duration={i.duration} role={i.role} work={i.work} />
+                )
+            })}
+
+            </> 
+
+        
+    )
+}
+
+
+export default IndividualExperience;
