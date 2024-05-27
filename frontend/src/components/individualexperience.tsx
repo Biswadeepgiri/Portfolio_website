068b5b53117ba20ea9b0e65fced477c744a44eef
Biswadeepgiri <biswadeepgiri48@gmail.com>
2024-05-27 11:02:47 +0530

    created the experiences section of the website

--- frontend/src/components/individualexperience.tsx
+++ frontend/src/components/individualexperience.tsx
@@ -12,15 +12,15 @@ type experience= {
 
 function AnotherComponent(prop: {company_logo:object,duration:string,role:string,work:string[]}):JSX.Element {
     return (
-        <div className="bg-gray-600 w-4/5 h-72 m-10 p-10 rounded-2xl flex flex-row space-x-5 hover:scale-105" >
+        <div className="bg-gray-600 w-4/5 h-72 m-10 p-10 rounded-2xl flex flex-row space-x-5 hover:scale-[1.03] transform-gpu" >
             
-        <img src={prop.company_logo} alt="company logo" />
+        <img src={prop.company_logo} className="h-56 w-56" alt="company logo" />
         <div className="flex flex-col">
-            <div className="flex flex-row">
+            <div className="flex flex-row justify-between my-3">
             <h1 className="text-white text-xl">{prop.role}</h1>
             <h1 className="text-white text-xl">{prop.duration}</h1>
             </div>
-            <ol>
+            <ol className="list-disc mx-3">
                 {prop.work.map(function(i){
                     return (
                         <li className="text-white">{i}</li>
